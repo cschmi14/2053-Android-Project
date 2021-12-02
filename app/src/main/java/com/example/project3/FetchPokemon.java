4bed23b608cc5e82b625545e8fcbb3d21f7c1323
jack-ahmed <rahmed6@villanova.edu>
2021-12-02 10:17:14 -0500

    started json parser

---
+++ app/src/main/java/com/example/project3/FetchPokemon.java
@@ -0,0 +1,104 @@
+package com.example.project3;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FetchPokemon extends AsyncTask<String, Void, String> {
+    private WeakReference<TextView> mName;
+    private WeakReference<ImageView> mIcon;
+    private WeakReference<TextView> mStats;
+    private WeakReference<TextView> mDescription;
+    private WeakReference<TextView> mType;
+    private WeakReference<TextView> mDexNumber;
+
+    FetchPokemon(TextView mName, TextView mStats, TextView mDescription, TextView mType, TextView mDexNumber, ImageView mIcon) {
+        this.mName = new WeakReference<>(mName);
+        this.mStats = new WeakReference<>(mStats);
+        this.mDescription = new WeakReference<>(mDescription);
+        this.mType = new WeakReference<>(mType);
+        this.mDexNumber = new WeakReference<>(mDexNumber);
+        this.mIcon = new WeakReference<>(mIcon);
+    }
+
+    protected String getPokemonInfo(String query) throws IOException {
+        //Pokemon API URL
+        String apiURL = "https://pokeapi.co/";
+        //Append query
+        apiURL += query;
+
+        //Make connection to API
+        URL requestURL = new URL(apiURL);
+        HttpURLConnection urlConnection = (HttpURLConnection) requestURL.openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.connect();
+
+        //Receive the response
+        InputStream inputStream = urlConnection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        //Create a String with the reponse
+        StringBuilder builder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+            builder.append("\n");
+        }
+        String jsonString = builder.toString();
+        Log.d("FetchBookTagJsonString", jsonString);
+        return jsonString;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String jsonString = null;
+        try {
+            jsonString = getPokemonInfo(strings[0]);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
+    }
+
+    protected void onPostExecute(String x) {
+        super.onPostExecute(x);
+        String mName = null;
+        String mDescription = null;
+        String mType = null;
+        String mDexNumber = null;
+        String mIcon = null;
+        String[] mStats = null;
+        JSONObject jsonObject = null;
+        JSONArray itemsArray = null;
+        int i = 0;
+        try {
+            jsonObject = new JSONObject(x);
+            itemsArray = new jsonObject.getJSONArray("items");
+            while (i<itemsArray.length() && mName == null && mDescription == null && mType == null && mDexNumber == null && mIcon == null && mStats == null) {
+                JSONObject pokemon = itemsArray.getJSONObject(i);
+                JSONObject dex = pokemon.getJSONObject("dex");
+                title = volumeInfo.getString("title");
+                author = volumeInfo.getString("authors");
+                mName.get().setText(mName);
+                mTitleText.get().setText(title);
+                i++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
--- app/src/main/java/com/example/project3/Pokemon.java
+++ app/src/main/java/com/example/project3/Pokemon.java
@@ -1,2 +1,20 @@
-package com.example.project3;public class Pokemon {
+package com.example.project3;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+public class Pokemon {
+    private WeakReference<TextView> mName;
+    private WeakReference<ImageView> mIcon;
+    private WeakReference<TextView> mHP;
+    private WeakReference<TextView> mSpAtk;
+    private WeakReference<TextView> mAttack;
+    private WeakReference<TextView> mSpDef;
+    private WeakReference<TextView> mDefense;
+    private WeakReference<TextView> mSpeed;
+    private WeakReference<TextView> mDescription;
+    private WeakReference<TextView> mType;
+    private WeakReference<TextView> mDexNumber;
 }
